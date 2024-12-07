50ef0e336e3d0cc5ea32bfe9f3b685eff4412d76
Dan Whettam <33871216+DWhettam@users.noreply.github.com>
2024-12-07 23:16:58 +0000

    Update day_2.py

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -3,19 +3,6 @@ with open('input.txt', 'r') as file:
 
 lines = [line.strip().split(' ') for line in lines]
 
-#lines = [
-#    [48, 46, 47, 49, 51, 54, 56],
-#    [1, 1, 2, 3, 4, 5],
-#    [1, 2, 3, 4, 5, 5],
-#    [5, 1, 2, 3, 4, 5],
-#    [1, 4, 3, 2, 1],
-#    [1, 6, 7, 8, 9],
-#    [1, 2, 3, 4, 3],
-#    [9, 8, 7, 6, 7],
-#    [7, 10, 8, 10, 11],
-#    [29, 28, 27, 25, 26, 25, 22, 20]
-#]
-
 def is_safe(line):
     ascending = True
     descending = True
